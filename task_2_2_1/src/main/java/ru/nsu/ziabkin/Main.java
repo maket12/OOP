2a5032dc615150c434bce81d8d3447c0b3b7310b
Зябкин Владимир <vovvovin5124@gmail.com>
2026-04-12 20:30:59 +0700

    fixes

--- task_2_2_1/src/main/java/ru/nsu/ziabkin/Main.java
+++ task_2_2_1/src/main/java/ru/nsu/ziabkin/Main.java
@@ -1,7 +1,10 @@
 package ru.nsu.ziabkin;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import ru.nsu.ziabkin.employers.Baker;
@@ -17,51 +20,70 @@ import ru.nsu.ziabkin.utils.PizzeriaConfig;
 public class Main {
     /**
      * Main method.
-     *
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        try {
+            runPizzeria();
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: Configuration file 'config.json' not found.");
+        } catch (JsonSyntaxException e) {
+            System.err.println("Error: Failed to parse JSON configuration. Check the file format.");
+        } catch (InterruptedException e) {
+            System.err.println("Main thread was interrupted.");
+            Thread.currentThread().interrupt();
+        } catch (Exception e) {
+            System.err.println("An unexpected error occurred: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static void runPizzeria() throws IOException, InterruptedException {
         Gson gson = new Gson();
-        PizzeriaConfig config = gson.fromJson(new FileReader("config.json"), PizzeriaConfig.class);
+        try (FileReader reader = new FileReader("config.json")) {
+            PizzeriaConfig config = gson.fromJson(reader, PizzeriaConfig.class);
 
-        OrderQueue queue = new OrderQueue();
-        Warehouse warehouse = new Warehouse(config.warehouseCapacity);
+            if (config == null) {
+                throw new IOException("Config file is empty");
+            }
 
-        List<Thread> bakerThreads = new ArrayList<>();
-        List<Thread> courierThreads = new ArrayList<>();
+            OrderQueue queue = new OrderQueue();
+            Warehouse warehouse = new Warehouse(config.warehouseCapacity);
 
-        for (int speed : config.bakerSpeeds) {
-            Thread t = new Thread(new Baker(speed, queue, warehouse));
-            t.start();
-            bakerThreads.add(t);
+            List<Thread> bakerThreads = new ArrayList<>();
+            List<Thread> courierThreads = new ArrayList<>();
+
+            for (int speed : config.bakerSpeeds) {
+                Thread t = new Thread(new Baker(speed, queue, warehouse));
+                t.start();
+                bakerThreads.add(t);
+            }
+
+            for (PizzeriaConfig.CourierConfig cc : config.couriers) {
+                Thread t = new Thread(new Courier(cc.trunkSize, cc.speedMs, warehouse));
+                t.start();
+                courierThreads.add(t);
+            }
+
+            for (int i = 1; i <= 15; i++) {
+                queue.addOrder(new Order(i));
+                Thread.sleep(200);
+            }
+
+            System.out.println("--- PIZZERIA STOPS ACCEPTANCE OF NEW ORDERS ---");
+            queue.close();
+
+            for (Thread t : bakerThreads) {
+                t.join();
+            }
+
+            System.out.println("--- ALL BAKERS HAVE FINISHED WORK, WAREHOUSE CLOSING ---");
+            warehouse.close();
+
+            for (Thread t : courierThreads) {
+                t.join();
+            }
+
+            System.out.println("--- THE WORKING DAY IS ENDED ---");
         }
-
-        for (PizzeriaConfig.CourierConfig cc : config.couriers) {
-            Thread t = new Thread(new Courier(cc.trunkSize, cc.speedMs, warehouse));
-            t.start();
-            courierThreads.add(t);
-        }
-
-        for (int i = 1; i <= 15; i++) {
-            queue.addOrder(new Order(i));
-            Thread.sleep(200);
-        }
-
-        System.out.println("--- PIZZERIA STOPS ACCEPTANCE OF NEW ORDERS ---");
-
-        queue.close();
-
-        for (Thread t : bakerThreads) {
-            t.join();
-        }
-
-        System.out.println("--- ALL BAKERS HAVE FINISHED WORK, WAREHOUSE CLOSING ---");
-
-        warehouse.close();
-
-        for (Thread t : courierThreads) {
-            t.join();
-        }
-
-        System.out.println("--- THE WORKING DAY IS ENDED ---");
     }
 }
